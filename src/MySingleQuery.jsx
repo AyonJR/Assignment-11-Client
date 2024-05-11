cffcb0f1b8d8d8c23ab17d48d7b025a228809118
unknown <abdulbaset.ayon@gmail.com>
2024-05-11 10:43:35 +0600

    done with the delete functionality

--- src/MySingleQuery.jsx
+++ src/MySingleQuery.jsx
@@ -1,7 +1,9 @@
+import axios from "axios";
 
 const MySingleQuery = ({mySingleQuery}) => { 
 
-    const { productName,
+    const { _id , 
+        productName,
         productBrand,
         imageUrl,
         queryTitle,
@@ -13,7 +15,24 @@ const MySingleQuery = ({mySingleQuery}) => {
         recommendation } = mySingleQuery;
 
 
+    const handleDelete = async id => {
+   
+    
+        const {data} = await axios.delete(`http://localhost:5000/query/${id}`)
+    
+
+
+
+    }
+
+
+
+
     return (
+        
+
+
+        //card
         <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
         <div className="w-full h-64 object-center bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{backgroundImage: `url(${imageUrl})`}}></div>
     
@@ -22,7 +41,7 @@ const MySingleQuery = ({mySingleQuery}) => {
     
             <div className="flex items-center justify-between px-3 py-2 bg-cyan-800 dark:bg-cyan-800">
             <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Update</button>
-                <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Delete</button>
+                <button onClick={()=> handleDelete(_id)} className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Delete</button>
             </div>
         </div>
     </div>
